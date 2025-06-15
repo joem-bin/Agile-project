5c1437c47ade3116f85451bff3fe2fd614e325bf
Joseph M <Joseph.M_2023_Man@outlook.com>
2025-06-15 13:52:12 +0100

    add crud tests and schema

--- tests/test_routes.py
+++
@@ -1,4 +0,0 @@
-def test_homepage_loads(client):
-    response = client.get('/')
-    assert response.status_code == 200
-    assert b"Login" in response.data  # Optional: make sure login form is shown
